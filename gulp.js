f1b91433fd1997ea58d1bc6ae52fb8952aaa3a57
Muhammad Tsani Qudsi <tsany@rokutsan.com>
2014-10-24 20:33:02 +0700

    Move Layout dll, sekalian package.json

--- gulp.js
+++ gulp.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
 
-gulp.task('default', function() {
-  
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+
+  gulp.src('./lib/*.jade')
+    .pipe(jade({
+      locals: JSON.parse(fs.readFileSync(o.views.data, 'utf8'))
+    }))
+    .pipe(gulp.dest('./dist/'))
 });
