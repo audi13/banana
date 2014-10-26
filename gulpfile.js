0f6f396fe0672fbe56be0a90cf6a2a2c5ead2b0b
Muhammad Tsani Qudsi <tsany@lokalhos.com>
2014-10-26 21:56:22 +0700

    gulp change again
    huah

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('../');
 
-gulp.task('default', function() {
-  var YOUR_LOCALS = {};
-
-  gulp.src('./lib/*.jade')
-    .pipe(jade({
-      locals: YOUR_LOCALS
-    }))
-    .pipe(gulp.dest('dist/'))
+gulp.task('default', ['jade', 'bootstrap'] {
+  
 });
+
+//- Get one .styl file and render
+gulp.task('bootstrap', function () {
+    gulp.src('bower_components/bootstrap-stylus/stylus/bootstrap.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('dist/assets/css/'));
+});
+
+// Set linenos
+gulp.task('linenos', function () {
+    gulp.src('./css/linenos.styl')
+        .pipe(stylus({linenos: true}))
+        .pipe(gulp.dest('./css/build'));
+});
+
+gulp.task('jade', function () {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./lib/*.jade')
+        .pipe(jade({
+            locals: YOUR_LOCALS
+        }))
+        .pipe(gulp.dest('dist/'))
+}
